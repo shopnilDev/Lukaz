ebf91d8eec9c9f420b3045c3eb32bbee57dcca59
AhmadShopnil <ahmadshopnil75@gmail.com>
2026-02-23 12:47:51 +0600

    fix

--- src/components/home/CategoriesGrid.jsx
+++ src/components/home/CategoriesGrid.jsx
@@ -29,29 +29,30 @@ const shopbyCategories=await getShopByGender();
 
       <div className="mx-auto max-w-[1640px] px-3 sm:px-4 md:px-16 grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4">
         {shopbyCategories?.slice(0, 20).map((category, idx) => (
+         
 
           <div
           key={idx}
             className="bg-white group h-[300px] md:h-[300px] lg:h-[330px] xl:h-[340px] rounded-md bg-cover bg-center relative overflow-hidden cursor-pointer">
             <Link
           
-              href={`/shop/${category?.name?.slug}`}
+              href={`/shop/${category?.url || category?.categories[0]?.slug }`}
               className="absolute inset-0 bg-center bg-cover transition-transform duration-700 scale-100 group-hover:scale-105"
-              style={{ backgroundImage: `url(${getImageUrl("category", category?.name?.thumbnail)})` }}
+              style={{ backgroundImage: `url(${getImageUrl("shopby", category?.thumbnail)})` }}
             ></Link>
             {/* dark overlay */}
             <Link
             
-              href={`/shop/${category?.name?.slug}`}
+              href={`/shop/${category?.url || category?.categories[0]?.slug}`}
               className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent">
 
             </Link>
 
             <Link
          
-              href={`/shop/${category?.name?.slug}`}
+              href={`/shop/${category?.url || category?.categories[0]?.slug}`}
               className="absolute text-center bottom-7 left-4 right-4 text-[#3A9E75] text-xl font-bold space-grotesk">
-              {category?.name?.name?.toUpperCase()}
+              {category?.name?.toUpperCase()}
             </Link>
           </div>
 
